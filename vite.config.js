eecbf500f5baf16976140acad146fbda289fd9de
King aruthr <aksdgfkausgfailsf@gmail.com>
2025-04-24 11:06:53 +0200

    Inits project.

---
+++ vite.config.js
@@ -0,0 +1,39 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath, URL } from 'node:url';
+
+const host = "localhost";
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: 'resources/js/app.jsx',
+            refresh: true,
+        }),
+        react(),
+    ],
+    resolve: {
+        alias: {
+            '@js': fileURLToPath(new URL('./resources/js', import.meta.url)),
+        },
+    },
+    server: {
+        host: host,
+        port: 8787,
+        hmr: {
+            host: host,
+            clientPort: 8787,
+        },
+        watch: {
+            followSymlinks: false,
+            ignored: [
+                '/app/public/',
+                '/app/storage/',
+                '/app/tests/',
+                '/app/vendor/',
+                '/app/reports/**'
+            ]
+        }
+    }
+});
